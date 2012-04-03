871eb3749b9a2255eb989daacb41ec94ff7b4435
afif mohammed <askafif@y7mail.com>
2012-04-03 22:52:42 +1000

    merged default implementations with interface in one class file.

--- Querite/IAmQuery.cs
+++ Querite/IAmQuery.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace querite
 {
@@ -7,5 +8,36 @@ namespace querite
         IAmQuery<TModel, TSource> Count(Action<int> count);
         IAmQuery<TModel, TSource> Stale(Func<DateTimeOffset> asOf);
         TModel Apply(TSource source);
+    }
+
+    public abstract class AbstractQuery<TModel, TSource> : IAmQuery<TModel, TSource>
+    {
+        private ICollection<Action<int>> SetCountActions { get; set; }
+        protected Func<DateTimeOffset> StaleAsOf { get; private set; }
+        protected void SetCount(int count)
+        {
+            foreach (var setCount in SetCountActions)
+                setCount(count);
+        }
+
+        protected AbstractQuery()
+        {
+            Stale(asOf: () => DateTimeOffset.Now);
+            SetCountActions = new List<Action<int>>();
+        }
+
+        public IAmQuery<TModel, TSource> Stale(Func<DateTimeOffset> asOf)
+        {
+            StaleAsOf = asOf;
+            return this;
+        }
+
+        public abstract TModel Apply(TSource source);
+
+        public IAmQuery<TModel, TSource> Count(Action<int> count)
+        {
+            SetCountActions.Add(count);
+            return this;
+        }
     }
 }
